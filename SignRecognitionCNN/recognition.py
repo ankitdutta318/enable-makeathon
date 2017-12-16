6dfc9a7cb2e0d9604691101d93d1146313093fa2
cruxbreaker <geekankit318@gmail.com>
2017-12-17 00:27:15 +0530

    Code Refactoring into train.py and recognition.py

--- SignRecognitionCNN/recognition.py
+++ SignRecognitionCNN/recognition.py
@@ -6,21 +6,16 @@ Created on Fri Dec 15 11:08:20 2017
 @author: cruxbreaker
 """
 # Import libraries
-import os,cv2
+import os, cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.utils import shuffle
 from sklearn.cross_validation import train_test_split
 
+from keras.utils import np_utils
 from keras import backend as K
 K.set_image_dim_ordering('th')
-
-from keras.utils import np_utils
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD,RMSprop,adam
 
 #%%
 
@@ -67,60 +62,7 @@ else:
 	if K.image_dim_ordering()=='th':
 		img_data=np.rollaxis(img_data,3,1)
 		print (img_data.shape)
-		
-#%%
-USE_SKLEARN_PREPROCESSING=False
-
-if USE_SKLEARN_PREPROCESSING:
-	# using sklearn for preprocessing
-	from sklearn import preprocessing
-	
-	def image_to_feature_vector(image, size=(128, 128)):
-		# resize the image to a fixed size, then flatten the image into
-		# a list of raw pixel intensities
-		return cv2.resize(image, size).flatten()
-	
-	img_data_list=[]
-	for dataset in data_dir_list:
-		img_list=os.listdir(data_path+'/'+ dataset)
-		print ('Loaded the images of dataset-'+'{}\n'.format(dataset))
-		for img in img_list:
-			input_img=cv2.imread(data_path + '/'+ dataset + '/'+ img )
-			input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-			input_img_flatten=image_to_feature_vector(input_img,(128,128))
-			img_data_list.append(input_img_flatten)
-	
-	img_data = np.array(img_data_list)
-	img_data = img_data.astype('float32')
-	print (img_data.shape)
-	img_data_scaled = preprocessing.scale(img_data)
-	print (img_data_scaled.shape)
-	
-	print (np.mean(img_data_scaled))
-	print (np.std(img_data_scaled))
-	
-	print (img_data_scaled.mean(axis=0))
-	print (img_data_scaled.std(axis=0))
-	
-	if K.image_dim_ordering()=='th':
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
-		print (img_data_scaled.shape)
-		
-	else:
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
-		print (img_data_scaled.shape)
-	
-	
-	if K.image_dim_ordering()=='th':
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],num_channel,img_rows,img_cols)
-		print (img_data_scaled.shape)
-		
-	else:
-		img_data_scaled=img_data_scaled.reshape(img_data.shape[0],img_rows,img_cols,num_channel)
-		print (img_data_scaled.shape)
-
-if USE_SKLEARN_PREPROCESSING:
-	img_data=img_data_scaled
+        
 #%%
 # Assigning Labels
 
@@ -187,115 +129,34 @@ Y = np_utils.to_categorical(labels, num_classes)
 x,y = shuffle(img_data,Y, random_state=2)
 # Split the dataset
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
-
+	
 #%%
-# Defining the model
-input_shape=img_data[0].shape
-					
-model = Sequential()
+# Saving and loading model and weights
+from keras.models import model_from_json
+from keras.models import load_model
 
-model.add(Convolution2D(32, 3,3,border_mode='same',input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(Convolution2D(32, 3, 3))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("model.hdf5")
+print("Loaded model from disk")
 
-model.add(Convolution2D(64, 3, 3))
-model.add(Activation('relu'))
-#model.add(Convolution2D(64, 3, 3))
-#model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.5))
-
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-
-#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-#model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
-
-# Viewing model_configuration
-
-model.summary()
-model.get_config()
-model.layers[0].get_config()
-model.layers[0].input_shape			
-model.layers[0].output_shape			
-model.layers[0].get_weights()
-np.shape(model.layers[0].get_weights()[0])
-model.layers[0].trainable
-
+loaded_model=load_model('model.hdf5')
 #%%
-# Training
-hist = model.fit(X_train, y_train, batch_size=16, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test))
-
-#hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=20,verbose=1, validation_split=0.2)
-
-#%%
-# Training with callbacks
-from keras import callbacks
-
-filename='model_train.csv'
-csv_log=callbacks.CSVLogger(filename, separator=',', append=False)
-
-early_stopping=callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='min')
-
-filepath="Best-weights-my_model-{epoch:03d}-{loss:.4f}-{acc:.4f}.hdf5"
-
-checkpoint = callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-
-callbacks_list = [csv_log,early_stopping,checkpoint]
-
-hist = model.fit(X_train, y_train, batch_size=16, nb_epoch=num_epoch, verbose=1, validation_data=(X_test, y_test),callbacks=callbacks_list)
-
-#%%
-# visualizing losses and accuracy
-train_loss=hist.history['loss']
-val_loss=hist.history['val_loss']
-train_acc=hist.history['acc']
-val_acc=hist.history['val_acc']
-xc=range(3)
-
-plt.figure(1,figsize=(7,5))
-plt.plot(xc,train_loss)
-plt.plot(xc,val_loss)
-plt.xlabel('num of Epochs')
-plt.ylabel('loss')
-plt.title('train_loss vs val_loss')
-plt.grid(True)
-plt.legend(['train','val'])
-#print plt.style.available # use bmh, classic,ggplot for big pictures
-plt.style.use(['classic'])
-
-plt.figure(2,figsize=(7,5))
-plt.plot(xc,train_acc)
-plt.plot(xc,val_acc)
-plt.xlabel('num of Epochs')
-plt.ylabel('accuracy')
-plt.title('train_acc vs val_acc')
-plt.grid(True)
-plt.legend(['train','val'],loc=4)
-#print plt.style.available # use bmh, classic,ggplot for big pictures
-plt.style.use(['classic'])
-
-#%%
-
 # Evaluating the model
 
-score = model.evaluate(X_test, y_test, verbose=0)
+score = loaded_model.evaluate(X_test, y_test, verbose=0)
 print('Test Loss:', score[0])
 print('Test accuracy:', score[1])
 
 test_image = X_test[0:1]
 print (test_image.shape)
 
-print(model.predict(test_image))
-print(model.predict_classes(test_image))
+print(loaded_model.predict(test_image))
+print(loaded_model.predict_classes(test_image))
 print(y_test[0:1])
 
 # Testing a new image
@@ -327,23 +188,23 @@ else:
 		print (test_image.shape)
 		
 # Predicting the test image
-print((model.predict(test_image)))
-print(model.predict_classes(test_image))
+print((loaded_model.predict(test_image)))
+print(loaded_model.predict_classes(test_image))
 
 #%%
 
 # Visualizing the intermediate layer
 
 #
-def get_featuremaps(model, layer_idx, X_batch):
-	get_activations = K.function([model.layers[0].input, K.learning_phase()],[model.layers[layer_idx].output,])
+def get_featuremaps(loaded_model, layer_idx, X_batch):
+	get_activations = K.function([loaded_model.layers[0].input, K.learning_phase()],[loaded_model.layers[layer_idx].output,])
 	activations = get_activations([X_batch,0])
 	return activations
 
 layer_num=3
 filter_num=0
 
-activations = get_featuremaps(model, int(layer_num),test_image)
+activations = get_featuremaps(loaded_model, int(layer_num),test_image)
 
 print (np.shape(activations))
 feature_maps = activations[0][0]      
@@ -376,36 +237,36 @@ fig.savefig("featuremaps-layer-{}".format(layer_num) + '.jpg')
 from sklearn.metrics import classification_report,confusion_matrix
 import itertools
 
-Y_pred = model.predict(X_test)
+Y_pred = loaded_model.predict(X_test)
 print(Y_pred)
 y_pred = np.argmax(Y_pred, axis=1)
 print(y_pred)
-#y_pred = model.predict_classes(X_test)
+#y_pred = loaded_model.predict_classes(X_test)
 #print(y_pred)
 target_names = ['class 0(gesture_t)', 
                 'class 1(gesture_o)', 
                 'class 2(gesture_f)',
                 'class 3(gesture_p)',
-                'class 1(gesture_x)',
-                'class 1(gesture_y)',
-                'class 1(gesture_u)',
-                'class 1(gesture_b)',
-                'class 1(gesture_q)',
-                'class 1(gesture_l)',
-                'class 1(gesture_c)',
-                'class 1(gesture_d)',
-                'class 1(gesture_e)',
-                'class 1(gesture_k)',
-                'class 1(gesture_w)',
-                'class 1(gesture_a)',
-                'class 1(gesture_m)',
-                'class 1(gesture_g)',
-                'class 1(gesture_j)',
-                'class 1(gesture_n)',
-                'class 1(gesture_h)',
-                'class 1(gesture_s)',
-                'class 1(gesture_v)',
-                'class 1(gesture_i)']
+                'class 4(gesture_x)',
+                'class 5(gesture_y)',
+                'class 6(gesture_u)',
+                'class 7(gesture_b)',
+                'class 8(gesture_q)',
+                'class 9(gesture_l)',
+                'class 10(gesture_c)',
+                'class 11(gesture_d)',
+                'class 12(gesture_e)',
+                'class 13(gesture_k)',
+                'class 14(gesture_w)',
+                'class 15(gesture_a)',
+                'class 16(gesture_m)',
+                'class 17(gesture_g)',
+                'class 18(gesture_j)',
+                'class 19(gesture_n)',
+                'class 20(gesture_h)',
+                'class 21(gesture_s)',
+                'class 22(gesture_v)',
+                'class 23(gesture_i)']
 
 					
 print(classification_report(np.argmax(y_test,axis=1), y_pred,target_names=target_names))
@@ -463,28 +324,3 @@ plot_confusion_matrix(cnf_matrix, classes=target_names,
 #                      title='Normalized confusion matrix')
 #plt.figure()
 plt.show()
-
-#%%
-# Saving and loading model and weights
-from keras.models import model_from_json
-from keras.models import load_model
-
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
-
-# load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-
-model.save('model.hdf5')
-loaded_model=load_model('model.hdf5')
